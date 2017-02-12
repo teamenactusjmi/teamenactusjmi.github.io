d7da73726ee4d57e5d68641ee092c70ca046e46a
Ajitesh Rai <ajiteshr7@gmail.com>
2017-02-12 18:06:44 +0530

    Added loading animation

---
+++ js/my.js
@@ -0,0 +1,7 @@
+$('#container').imagesLoaded( function() {
+    $(".se-pre-con").fadeOut(500);
+});
+// .progress( function( instance, image ) {
+//     var result = image.isLoaded ? 'loaded' : 'broken';
+//     console.log( 'image is ' + result + ' for ' + image.img.src );
+//   });
